d93309ca9b88aa94fd380ef94b37de8b3a3e14ac
quartzeast <quartzesat@gmail.com>
2025-04-24 12:14:47 +0800

    添加 output_parser，完成前端

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,18 +1,21 @@
+from typing import Tuple
 from dotenv import load_dotenv
-from langchain.prompts.prompt import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain.chat_models import init_chat_model
 
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.twitter import scrape_user_tweets
 
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_user_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_user_profile_url,
         scrapin=False,
+        mock=False,
     )
 
     twitter_username = twitter_lookup_agent(name=name)
@@ -25,17 +28,23 @@ def ice_break_with(name: str) -> str:
     2. two interesting facts about them 
 
     Use both information from twitter and Linkedin
+    \n{format_instructions}
     """
 
-    summary_prompt_template = PromptTemplate.from_template(summary_template)
+    summary_prompt_template = PromptTemplate.from_template(
+        summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions(),
+        },
+    )
 
     llm = init_chat_model("gpt-4o-mini", model_provider="openai", temperature=0)
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
